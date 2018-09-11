420d42d27af7f72271dc25f14d3e9345466585ea
srachha <sandeep@westagilelabs.com>
2018-09-11 16:38:44 +0530

    Project Setup.

---
+++ BKI/BKI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BKI
+//
+//  Created by srachha on 11/09/18.
+//  Copyright © 2018 srachha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
