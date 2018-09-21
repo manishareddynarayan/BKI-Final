273cf6db68abb37b8dcb5954bab8424a11371879
srachha <sandeep@westagilelabs.com>
2018-09-21 17:01:41 +0530

    Code beat changes.

--- BKI/BKI/ViewControllers/Root/BaseViewController.swift
+++ BKI/BKI/ViewControllers/Root/BaseViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class BaseViewController: UIViewController {
 
-    let appDelegate = UIApplication.shared.delegate as? AppDelegate
+    weak var appDelegate = UIApplication.shared.delegate as? AppDelegate
     var sessionManager = AUSessionManager.shared
     var alertVC = RBAAlertController()
     let defs = BKIModel.initUserdefsWithSuitName()
@@ -26,22 +26,19 @@ class BaseViewController: UIViewController {
         bgImageview.frame = self.view.bounds
         self.view.addSubview(bgImageview)
         self.view.sendSubview(toBack: self.bgImageview)
-        //self.setConstraintsForBGImageView()
         // Do any additional setup after loading the view.
     }
 
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        //
-
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    func hideNavigationController() -> Void {
+    func hideNavigationController() {
         switch type(of: self) {
         case is MainDashBoardVC.Type, is DashBoardVC.Type:
             self.navigationController?.isNavigationBarHidden = true
@@ -64,14 +61,5 @@ class BaseViewController: UIViewController {
     @IBAction func moreAction(_ sender: Any) {
         self.navigationController?.popToRootViewController(animated: true)
     }
-
-    func setConstraintsForBGImageView() -> Void {
-        let topConstraint = NSLayoutConstraint.init(item: self.bgImageview, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1.0, constant: 0.0)
-        let bottombConstraint = NSLayoutConstraint.init(item: self.bgImageview, attribute: .bottom, relatedBy: .equal, toItem: self.view, attribute: .bottom, multiplier: 1.0, constant: 0.0)
-        let leadingConstraint = NSLayoutConstraint.init(item: self.bgImageview, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1.0, constant: 0.0)
-        let trailingbConstraint = NSLayoutConstraint.init(item: self.bgImageview, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1.0, constant: 0.0)
-       
-       self.view.translatesAutoresizingMaskIntoConstraints = false
-   self.view.addConstraints([topConstraint,bottombConstraint,leadingConstraint,trailingbConstraint])
-    }
+    
 }
