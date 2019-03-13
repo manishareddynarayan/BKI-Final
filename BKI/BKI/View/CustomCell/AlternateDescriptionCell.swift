b68f8b231d0eaf1d519bd1d2eda9a8c64fcb11f9
Anchal Kumar Gupta <anchal.g@westagilelabs.com>
2019-03-13 17:47:03 +0530

    created model for alternate description, bug fix

--- BKI/BKI/View/CustomCell/AlternateDescriptionCell.swift
+++ BKI/BKI/View/CustomCell/AlternateDescriptionCell.swift
@@ -9,26 +9,17 @@
 import UIKit
 
 class AlternateDescriptionCell: BaseCell {
-
+    
     @IBOutlet weak var descriptionLbl: UILabel!
     @IBOutlet weak var notesLbl: UILabel!
     @IBOutlet weak var sizeLbl: UILabel!
     @IBOutlet weak var headingLbl: UILabel!
     
-    func setCell(data:[[String:AnyObject]], index:Int){
-        if data.count>1{
-            if let heading = data[index + 1]["key"] as? String{
-                headingLbl.text = heading.uppercased()
-            }
-            if let size = data[index + 1]["size"] as? String{
-                sizeLbl.text = size
-            }
-            if let notes = data[index + 1]["item_notes"] as? String{
-                notesLbl.text = notes
-            }
-            if let description = data[index + 1]["alternate_description"] as? String{
-                descriptionLbl.text = description
-            }
-        }
+    func setCell(data:AlternateDescription){
+        headingLbl.text = data.heading!
+        sizeLbl.text = data.size!
+        notesLbl.text = data.notes!
+        descriptionLbl.text = data.altDescription!
+        
     }
 }
